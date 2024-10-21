e679abee4577f09861ed0ad40afb8f3527e18cf5
sumit kumar <111673564+sumit-kumar-2104@users.noreply.github.com>
2024-10-21 19:52:02 +0530

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,16 @@
+from flask_login import UserMixin
+from extensions import db, login_manager  # Import from extensions
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
